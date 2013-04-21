809e029f4fc0c485c2968885bc604bb82731f8de
Anthony Schmieder <aschmied@uwaterloo.ca>
2013-04-20 22:12:47 -0400

    add a few command line arguments

--- main.py
+++ main.py
@@ -3,20 +3,33 @@ import time
 import threading
 
 from ui.assetmanager import Assets
-from ui.keyboard import Keyboard
 from ui.applicationwindow import ApplicationWindow
 from instrument.keyboard import KeyPressToMusicEventMapper
 from instrument import midi
 from instrumentstate import InstrumentState
-from sequencer import midiplayer
+from sequencer.midiplayer import MidiPlayer
 from playingsongstate import PlayingSongState
 
-if __name__ == '__main__':
+def usage():
+    print("Usage: keyzer runType songPath trackNumber")
 
+def parseArgs(argv):
+    if len(argv) != 4:
+        print(argv)
+        usage()
+        sys.exit(1)
+    runType = argv[1]
+    songPath = argv[2]
+    trackToLearn = argv[3]
+    return (runType, songPath, trackToLearn)
+
+def main(argv):
+
+    (runType, songPath, trackToLearn) = parseArgs(argv)
+
+    midiin = None
     Assets.loadAssets()
     app = ApplicationWindow()
-
-    runType = ""
     if runType == "k":
         keyPressToMusicEventMapper = KeyPressToMusicEventMapper()
         app.attachToKeyboard(keyPressToMusicEventMapper)
@@ -26,43 +39,15 @@ if __name__ == '__main__':
         midiin.attach(InstrumentState)
         midiin.openPort(midiin.probeMidiPorts()[0])
 
-        #song = ("/Users/anthonyschmieder/Dropbox/"
-                #"music/midi/midi_parts.mid")
-        #song = ("/Users/anthonyschmieder/Dropbox/music/midi/chet_atkins_windy_and_warm.mid")
-        #song = "/Users/anthonyschmieder/Dropbox/music/midi/bach_air.mid"
-        #song = "/Users/anthonyschmieder/Dropbox/music/midi/nine_inch_nails-closer.mid"
-        song = "/Users/anthonyschmieder/Dropbox/music/midi/beat.mid"
-        midiplayer = midiplayer.MidiPlayer(song)
+        midiplayer = MidiPlayer(songPath)
         midiplayer.attach(PlayingSongState)
         threading.Thread(target=midiplayer.play).start()
 
-
     app.start()
-    midiin.closePort()
-    
-    if False:
-    
-        midiout = midi.OutputConnection()
-        midiout.openPort(midiout.probeMidiPorts()[0])
-            
-        midiin = midi.InputConnection()
-        midiin.attach(InstrumentState)
-        midiin.openPort(midiin.probeMidiPorts()[0])    
-        
-        
-        while True:
-            midiout.sendNoteOnEvent(59, 80)
-            midiout.sendNoteOnEvent(41, 80)
-            time.sleep(.33)
-            midiout.sendNoteOffEvent(59, 0)
-            midiout.sendNoteOffEvent(41, 0)
-            time.sleep(.33)
-            midiout.sendNoteOnEvent(59, 80)
-            midiout.sendNoteOnEvent(40, 80)
-            time.sleep(.33)
-            midiout.sendNoteOffEvent(59, 0)
-            midiout.sendNoteOffEvent(40, 0)
-            time.sleep(.33)
-    
+    if midiin is not None:
         midiin.closePort()
-        midiout.closePort()
+    
+if __name__ == '__main__':
+    main(sys.argv)
+    sys.exit(0)
+
