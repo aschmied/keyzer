402f177ed0012cbeb2cabd4a8524836c6b8a83c2
Anthony Schmieder <anthony@primatelabs.com>
2013-10-03 22:56:26 -0400

    Makes SongScore draw only the correct staff lines

--- ui/songscore.py
+++ ui/songscore.py
@@ -1,6 +1,8 @@
 import pyglet
+
 from ui.assetmanager import Assets
-#from playingsongstate import PlayingSongState
+import util.music
+
 
 class StaffLine(pyglet.sprite.Sprite):
 
@@ -14,16 +16,23 @@ class StaffLine(pyglet.sprite.Sprite):
 
 class SongScore(pyglet.graphics.Batch):
 
+    notesWithStaffline = [22, 26, 29, 32, 36, 43, 46, 50, 53, 56]
+
     def __init__(self, x, y, lowAkeyCentreY, whiteKeyHeight):
         super(SongScore, self).__init__()
         self._x = x
         self._y = y
-        #self._noteheadOrigins = 88 * [-1]
-        whiteKeyIndexes = range(0, 52)
-        self._noteheadOrigins = [(x, lowAkeyCentreY + i*whiteKeyHeight)
-                                 for i in whiteKeyIndexes]
+        self._computeNoteheadOrigins(lowAkeyCentreY, whiteKeyHeight)
         self._getScoreAssets()
         self._drawStaff()
+
+    def _computeNoteheadOrigins(self, lowAkeyCentreY, whiteKeyHeight):
+        self._naturalNoteheadOrigins = [(self._x, lowAkeyCentreY)] + 87*[None]
+        noteheadYorigin = lowAkeyCentreY
+        for i in range(1, 88):
+            if not util.music.noteIndexIsSharp(i):
+                noteheadYorigin += whiteKeyHeight
+                self._naturalNoteheadOrigins[i] = (self._x, noteheadYorigin)
 
     def _getScoreAssets(self):
         self._staffLineImage = Assets.get("staff_line.png")
@@ -31,8 +40,10 @@ class SongScore(pyglet.graphics.Batch):
 
     def _drawStaff(self):
         self._staffLines = []
-        for (x, y) in self._noteheadOrigins[1::2]:
+        for noteIndex in SongScore.notesWithStaffline:
+            (x, y) = self._naturalNoteheadOrigins[noteIndex]
             self._staffLines.append(StaffLine(self._staffLineImage, (x, y), self))
         
     def update(self, dt):
         pass
+
