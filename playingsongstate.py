838809baccc013620b523f75f47ba3e3440c98c7
Anthony Schmieder <anthony@primatelabs.com>
2015-10-06 21:06:03 -0400

    Remove Queue.Queue in PlayingSongState
    Variable assignment is atomic and tread safe in Python, so the queue is not
    necessary (http://effbot.org/pyfaq/what-kinds-of-global-value-mutation-are-thread-safe.htm)

--- playingsongstate.py
+++ playingsongstate.py
@@ -7,7 +7,6 @@ class PlayingSongState():
     _notes = None
     _currentTick = 0
     _attached = []
-    _tickUpdateQueue = Queue.Queue()
     _log = logging.getLogger("keyzer:PlayingSongState")
 
     @staticmethod
@@ -32,25 +31,9 @@ class PlayingSongState():
     @staticmethod
     def getCurrentTick():
         PlayingSongState._log.debug("getCurrentTick()")
-        # The tick is the current time point in the playing song so when >1
-        # are in the queue the most recent is the most correct. Discard others.
-        try:
-            tick = None
-            while True:
-                tick = PlayingSongState._tickUpdateQueue.get_nowait()
-        except Queue.Empty:
-            if tick:
-                PlayingSongState._currentTick = tick
         return PlayingSongState._currentTick
 
     @staticmethod
     def onTickUpdate(tick):
         PlayingSongState._log.debug("onTickUpdate(tick={})".format(tick))
-        # Tick updates received from the song player. If consumers get so behind
-        # that the queue gets full then something is seriously wrong. Just 
-        # discard the events in that case.
-        try:
-            PlayingSongState._tickUpdateQueue.put_nowait(tick)
-        except Queue.Full:
-            PlayingSongState._log.warning("_tickUpdateQueue is full")
-
+        PlayingSongState._currentTick = tick
